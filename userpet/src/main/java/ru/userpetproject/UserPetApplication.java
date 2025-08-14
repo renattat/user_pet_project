2fed33cb9ed79b5e2c05a9af1f9db1144b96bf90
renat_sim <renat.tat@mail.ru>
2025-08-15 00:15:52 +0300

    Старт проекта

---
+++ userpet/src/main/java/ru/userpetproject/UserPetApplication.java
@@ -0,0 +1,13 @@
+package ru.userpetproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserPetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserPetApplication.class, args);
+	}
+
+}
